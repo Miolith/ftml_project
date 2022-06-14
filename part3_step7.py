49375fc4e15b6d8e2080f2819a0bade2d9e27c56
Nelson VICEL--FARAH <nelson.vicel-farah@epita.fr>
2022-06-14 13:07:43 +0200

    simulation part3

---
+++ part3_step7.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+n = 1000
+d = 20
+
+
+X = np.random.rand(n,d)
+
+theta_star = np.random.randint(1,d, size=d)
+y = X @ theta_star
+I_n = np.identity(n)
+
+
+sigma_2 = (1/(n-d) * np.linalg.norm(y - X @ np.linalg.inv(X.T @ X) @ X.T @ y) ** 2)
+
+print("sigma^2 estimation :", sigma_2)
