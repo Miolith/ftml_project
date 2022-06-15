cf2f37517ce710832ec461fd1c8b745772419ea9
Nelson VICEL--FARAH <nelson.vicel-farah@epita.fr>
2022-06-15 09:57:15 +0200

    clean part3 simulation

--- part3_step7.py
+++ part3_step7.py
@@ -8,8 +8,6 @@ X = np.random.rand(n,d)
 
 theta_star = np.random.randint(1,d, size=d)
 y = X @ theta_star
-I_n = np.identity(n)
-
 
 sigma_2 = (1/(n-d) * np.linalg.norm(y - X @ np.linalg.inv(X.T @ X) @ X.T @ y) ** 2)
 
