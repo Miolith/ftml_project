c3c558726d70e402a2e0f582d7a6a04943a2b000
Nelson VICEL--FARAH <nelson.vicel-farah@epita.fr>
2022-06-15 11:33:34 +0200

    change likelihood estimation with ols

--- part1_simulation.py
+++ part1_simulation.py
@@ -6,13 +6,12 @@ Y = np.array([np.random.exponential(1/(1 + x)) for x in X]).ravel()
 def bayes_estimator(x):
     return 1/(1+x)
 
-def likelihood_estimator(x):
-    l = 1 / np.mean(Y)
-    return l * np.exp(-l * x)
+def ols_estimator(x):
+    return (12 - 13 *np.log(2)) * x + 15/2 * np.log(2) - 6
 
 def compute_empirical_risk(f, X, Y):
     predictions = np.array([f(x) for x in X])
     return np.mean((Y - predictions) ** 2)
 
-print(compute_empirical_risk(bayes_estimator,X,Y))
-print( compute_empirical_risk(likelihood_estimator, X, Y))
+print("Bayes estimator loss", compute_empirical_risk(bayes_estimator,X,Y))
+print("OLS estimator loss", compute_empirical_risk(ols_estimator, X, Y))
