b122e7793d9f0f0e5bd03561e131641759cfd6be
Nelson VICEL--FARAH <nelson.vicel-farah@epita.fr>
2022-06-15 16:52:36 +0200

    added epsilon to y

--- part3_step7.py
+++ part3_step7.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-n = 1000
+n = 10000
 d = 20
 
 
@@ -11,4 +11,12 @@ y = X @ theta_star
 
 sigma_2 = (1/(n-d) * np.linalg.norm(y - X @ np.linalg.inv(X.T @ X) @ X.T @ y) ** 2)
 
-print("sigma^2 estimation :", sigma_2)
+print("sigma^2 estimation with epsilon=0:   %.2f" % sigma_2)
+
+# Now with epsilon noise
+
+epsilon = np.random.normal(0, 2, size=n)
+y = X @ theta_star + epsilon
+sigma_2 = (1/(n-d) * np.linalg.norm(y - X @ np.linalg.inv(X.T @ X) @ X.T @ y) ** 2)
+
+print("sigma^2 estimation with epsilon=N(mean:0, sigma:2):   %.2f" % sigma_2)
