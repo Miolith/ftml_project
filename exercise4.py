c4d6b1c0db567ab3015996c48493d8ac32b1a3d1
Nelson VICEL--FARAH <nelson.vicel-farah@epita.fr>
2022-06-17 20:10:17 +0200

    use of LassoCV to try better hyperparameter tuning

--- exercise4.py
+++ exercise4.py
@@ -1,11 +1,13 @@
 import numpy as np
 from sklearn.svm import SVR
-from sklearn.linear_model import Ridge, Lasso
+from sklearn.linear_model import RidgeCV, LassoCV, ElasticNetCV, Ridge, Lasso, ElasticNet
 from sklearn.ensemble import RandomForestRegressor, AdaBoostRegressor, GradientBoostingRegressor
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
 
+import warnings
+warnings.filterwarnings("ignore")
 
 X = np.load("data/regression/inputs.npy")
 y = np.load("data/regression/labels.npy")
@@ -24,9 +26,9 @@ print("Let's try some of them:\n")
 model_list = {
             "SVR Linear": SVR(kernel='linear'),
             "SVR rbf": SVR(kernel='rbf'),
-            "Ridge alpha=12.0": Ridge(alpha=12.0, random_state=0),
-            "Ridge alpha=15.0": Ridge(alpha=15.0, random_state=0),
-            "Lasso alpha=12.0": Lasso(alpha=12.0),
+            "RidgeCV": RidgeCV(),
+            "LassoCV": LassoCV(),
+            "ElasticNetCV": ElasticNetCV(),
             "RandomForestRegressor" : RandomForestRegressor(n_estimators=100, max_depth=15),
             "AdaBoost Regressor" : AdaBoostRegressor(n_estimators=100),
             "Gradient Boosting Regressor" : GradientBoostingRegressor(n_estimators=100)
@@ -35,10 +37,22 @@ model_list = {
 for (name, model) in model_list.items():
     print(name, "performance :", test_model(model))
 
-print("\nRidge regression seems to be the best regression model in this case")
-
 clf = GridSearchCV(estimator=Ridge(),
         param_grid={'alpha': range(1, 100)})
 clf.fit(X,y)
-print("The best parameters are :", clf.best_params_)
+print("The best parameters for Ridge are :", clf.best_params_)
+print("The CV score of this setting is", clf.best_score_)
+
+
+clf = GridSearchCV(estimator=Lasso(),
+        param_grid={'alpha': [.01, .05,.1, .2, .3, .4, .5, .6, .7 ,.8, .9, 1.]})
+clf.fit(X,y)
+print("The best parameters for Lasso are :", clf.best_params_)
+print("The CV score of this setting is", clf.best_score_)
+
+
+clf = GridSearchCV(estimator=ElasticNet(fit_intercept=True),
+        param_grid={'alpha': [0.0125, 0.025, 0.05, .125, .25, .5, 1., 2., 4.]})
+clf.fit(X,y)
+print("The best parameters for ElasticNet are :", clf.best_params_)
 print("The CV score of this setting is", clf.best_score_)
