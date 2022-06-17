4f80b6a9478719556d860d1d8db127e1aeec638e
Nelson VICEL--FARAH <nelson.vicel-farah@epita.fr>
2022-06-17 21:09:14 +0200

    vocab change part1 : loss => error

--- part1_simulation.py
+++ part1_simulation.py
@@ -13,5 +13,5 @@ def compute_empirical_risk(f, X, Y):
     predictions = np.array([f(x) for x in X])
     return np.mean((Y - predictions) ** 2)
 
-print("Bayes estimator loss", compute_empirical_risk(bayes_estimator,X,Y))
-print("OLS estimator loss", compute_empirical_risk(ols_estimator, X, Y))
+print("Bayes estimator error", compute_empirical_risk(bayes_estimator,X,Y))
+print("OLS estimator error", compute_empirical_risk(ols_estimator, X, Y))
